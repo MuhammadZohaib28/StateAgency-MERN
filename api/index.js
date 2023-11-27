39c95b06fbfdc9b3a08ed2c855ceda1964f2a479
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2023-11-27 22:23:50 +0500

    created a middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -22,6 +22,18 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
+
+// MiddleWare
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    error: message,
+  });
+});
